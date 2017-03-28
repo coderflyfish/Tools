9eab6d4ed6f5ebf91bbbbad611ac2526e07f9863
coderflyfish <277802345@qq.com>
2017-03-28 20:54:34 +0800

    111

--- Assets/Editor/DependReferenceChangeEditor.cs
+++ Assets/Editor/DependReferenceChangeEditor.cs
@@ -6,20 +6,24 @@ using NUnit.Framework.Constraints;
 using UnityEditor;
 using UnityEngine;
 using System.Collections;
+using Object = UnityEngine.Object;
 
 public class DependReferenceChangeEditor
 {
-    private const string SearchPath = "TestSearch";
+    private const string SearchPath = "ZZZ";
     [MenuItem("Assets/应用修改到全部依赖资源")]
     [MenuItem("UI/应用修改到全部依赖资源")]
     public static void OprDependReferncePrefabs()
     {
-       List<GameObject> replaceObjs = searchGameObjects();
+        List<string> needReplaceTags =new List<string>();
         GameObject[] objs = Selection.gameObjects;
         for (int i = 0; i < objs.Length; i++)
         {
-            OprDependReferncePrefab(objs[i], replaceObjs);
+            needReplaceTags.Add(objs[i].name);
         }
+        Dictionary<GameObject, Dictionary<string,List<string>>> replaceObjs = searchGameObjects(needReplaceTags);
+
+        OprDependReferncePrefab(objs, replaceObjs);
     }
 
     public static string[] GetPathsByDirectoryAndTail(string directory, string searchPattern)
@@ -34,103 +38,97 @@ public class DependReferenceChangeEditor
         return paths;
     }
 
-    private static List<GameObject> searchGameObjects( )
+    private static Dictionary<GameObject, Dictionary<string, List<string>>> searchGameObjects(List<string> tagList)
     {
-
-        List<GameObject> results = new List<GameObject>();
+        Dictionary<GameObject, Dictionary<string, List<string>>> results = new Dictionary<GameObject, Dictionary<string, List<string>>>();
         string[] paths = GetPathsByDirectoryAndTail(SearchPath,"*.prefab");
         for (int i = 0; i < paths.Length; i++)
         {
             GameObject obj = AssetDatabase.LoadAssetAtPath<GameObject>(paths[i]);
             if (obj != null)
             {
-                results.Add(obj);
+                Transform[] childs = obj.GetComponentsInChildren<Transform>(true);
+                for (int j = 0; j < childs.Length; j++)
+                {
+                    string tag = childs[j].gameObject.tag;
+                    if (tagList.Contains(tag))
+                    {
+                        if (!results.ContainsKey(obj))
+                        {
+                            results[obj] = new Dictionary<string, List<string>>();
+                        }
+                        if (!results[obj].ContainsKey(tag))
+                        {
+                            results[obj][tag] = new List<string>();
+                        }
+                        results[obj][tag].Add(childs[j].gameObject.name);
+                    }
+                }
+                
             }
         }
         return results;
     }
-    private static void OprDependReferncePrefab(GameObject prefab,List<GameObject> replaceObjs )
+    private static void OprDependReferncePrefab(GameObject[] objs, Dictionary<GameObject, Dictionary<string, List<string>>> replaceObjs)
     {
         if(replaceObjs==null||replaceObjs.Count==0)
-            return;
-       CMonoBehaviour[] cMono = prefab.GetComponentsInChildren<CMonoBehaviour>();
-       Dictionary<Type, CMonoBehaviour> needChanges = new Dictionary<Type, CMonoBehaviour>();
-       for (int i = 0; i < cMono.Length; i++)
-       {
-           if (cMono[i].ConnectRefPrefab)
-           {
-               needChanges.Add(cMono[i].GetType(), cMono[i]);
-           }
-       }
-        GameObject replaceInstance = null;
-       for (int j = 0; j < replaceObjs.Count; j++)
-       {
-           bool change = false;
-           if (prefab != replaceObjs[j])
-           {
-               replaceInstance =(GameObject)PrefabUtility.InstantiatePrefab(replaceObjs[j]);
-               CMonoBehaviour[] cMonoReplace = replaceInstance.GetComponentsInChildren<CMonoBehaviour>();
-               for (int i = 0; i < cMonoReplace.Length; i++)
-               {
-                   if (cMonoReplace[i].ConnectRefPrefab)
-                   {
-                       Type type = cMonoReplace[i].GetType();
-                       if (needChanges.ContainsKey(type))
-                       {
-                           change = true;
-                           GameObject instance = (GameObject)PrefabUtility.InstantiatePrefab(PrefabUtility.FindPrefabRoot(needChanges[type].gameObject));
-                           var components =instance.GetComponentsInChildren(type, true);
-                           string logStr = "使用 ";
-                           Component useCom = null;
-                           if (components.Length == 1)
-                           {
-                               useCom = components[0];
-                           }
-                           else
-                           {
-                               for (int k = 0; k < components.Length; k++)
-                               {
-                                   if (components[k].gameObject.name == cMonoReplace[i].name)
-                                   {
-                                       useCom = components[k];
-                                       
-                                       break;
-                                   }
-                               }
-                           }
-                           if (useCom != null)
-                           {
-                               logStr += instance.name + "的 " + useCom.gameObject.name + "替换了 " + replaceInstance.name +
-                                         "的" + cMonoReplace[i].name;
-                               Debug.Log(logStr);
-                               cMonoReplace[i] = ReplaceChangePart(useCom.transform, cMonoReplace[i].transform);
-                           }
+            return ;
+       
+        for (int i = 0; i < objs.Length; i++)
+        {
+            List<GameObject> changeObjs =new List<GameObject>();
+            string searchTag = objs[i].name;
+            foreach (var kv in replaceObjs)
+            {
+                if (kv.Value.ContainsKey(searchTag))
+                {
+                    changeObjs.Add(kv.Key);
+                }
+            }
+            for (int j = 0; j < changeObjs.Count; j++)
+            {
+                GameObject replaceObj = changeObjs[j];
+                GameObject target = (GameObject)PrefabUtility.InstantiatePrefab(replaceObj);
+                bool change = false;
+
+               
+                string logStr = "使用 ";
+                List<string> childs = replaceObjs[replaceObj][searchTag];
+                for (int k = 0; k < childs.Count; k++)
+                {
+
+                    Transform childGo = target.transform.FindChild(childs[k]);
+                    if (childGo!=null)
+                    {
+                        change = true;
+                        GameObject source = (GameObject)PrefabUtility.InstantiatePrefab(objs[i]);
+                        if (childGo != target.transform)
+                        {
+                            logStr += source.name + "替换了 " + target.name +
+                                    "的" + childGo.name;
+                            ReplaceChangePart(source.transform, childGo);
                            
-                           if (cMonoReplace[i].gameObject != instance)
-                           {
-                               GameObject.DestroyImmediate(instance);
-                           }
-                           if (cMonoReplace[i].transform.parent == null)
-                           {
-                               replaceInstance = cMonoReplace[i].gameObject;
-                               break;
-                           }
-                           
-                       }
-                   }
-               }
-               if (change)
-               {
-                   replaceObjs[j] = PrefabUtility.ReplacePrefab(replaceInstance, replaceObjs[j]);
-               }
-               AssetDatabase.SaveAssets();
-               GameObject.DestroyImmediate(replaceInstance);  
-              
-           }
-       }
+                            Debug.Log(logStr);
+                        }
+                    }
+                }
+
+                if (change)
+                {
+                   
+                    var catche = replaceObjs[replaceObj];
+                    replaceObjs.Remove(replaceObj);
+                    replaceObj = PrefabUtility.ReplacePrefab(target, replaceObj);
+                    replaceObjs.Add(replaceObj, catche);
+                }
+                AssetDatabase.SaveAssets();
+                GameObject.DestroyImmediate(target);  
+            }
+        }
+       
     }
 
-    private static CMonoBehaviour ReplaceChangePart(Transform source, Transform target)
+    private static void ReplaceChangePart(Transform source, Transform target)
     {
         PrefabUtility.DisconnectPrefabInstance(PrefabUtility.FindPrefabRoot(source.gameObject));
         source.parent = target.parent;
@@ -138,8 +136,8 @@ public class DependReferenceChangeEditor
         source.localRotation = target.localRotation;
         source.localScale = target.localScale;
         source.gameObject.name = target.gameObject.name;
-        GameObject.DestroyImmediate(target.gameObject); 
-        
-        return source.GetComponent<CMonoBehaviour>();
+        source.tag = target.tag;
+        source.SetSiblingIndex(target.GetSiblingIndex());
+        GameObject.DestroyImmediate(target.gameObject);
     }
 }
